cc6cba2af14efdb509de3461d247e5b784653c85
QeNo <moodiiy.98@gmail.com>
2022-03-11 15:49:06 +0200

    Add header and footer components

---
+++ src/components/Footer/Footer.component.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+    return (
+        <footer className='bg-light text-center text-lg-start'>
+            {/* <!-- Copyright --> */}
+            <div
+                className='text-center p-3'
+                style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}
+            >
+                © 2020 Copyright:
+                <a className='text-dark' href='https://mdbootstrap.com/'>
+                    MDBootstrap.com
+                </a>
+            </div>
+            {/* <!-- Copyright --> */}
+        </footer>
+    );
+};
+
+export default Footer;
