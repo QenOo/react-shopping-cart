068cd55b948933f37edc0484ca2c53224a6ec539
QeNo <moodiiy.98@gmail.com>
2022-03-14 23:59:01 +0200

    add filter component

---
+++ src/components/Filter/Filter.component.jsx
@@ -0,0 +1,38 @@
+const Filter = (props) => {
+    console.log(props);
+    return (
+        <form>
+            <div className='card p-4'>
+                <div className='form-group'>
+                    <span>Size</span>
+                    <select
+                        className='form-select'
+                        onChange={props.handleFilterBySize}
+                        value={props.size}
+                    >
+                        <option value='ALL'>All</option>
+                        <option value='S'>S</option>
+                        <option value='M'>M</option>
+                        <option value='L'>L</option>
+                        <option value='XL'>XL</option>
+                        <option value='XXL'>XXL</option>
+                    </select>
+                </div>
+                <div className='form-group'>
+                    <span>Sort by</span>
+                    <select
+                        className='form-select'
+                        onChange={props.handleFilterBySort}
+                        value={props.sort}
+                    >
+                        <option value='latest'>Latest</option>
+                        <option value='lowest'>Lowest</option>
+                        <option value='highest'>Highest</option>
+                    </select>
+                </div>
+            </div>
+        </form>
+    );
+};
+
+export default Filter;
